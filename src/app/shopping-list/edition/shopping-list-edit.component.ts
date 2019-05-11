f5fa49be696f710a304c660b8213f097f0f1a5fa
Jorge Solís <jpsb94@comunidad.unam.mx>
2019-05-10 19:39:57 -0500

    Se pueden agregar ingredientes

--- src/app/shopping-list/edition/shopping-list-edit.component.ts
+++ src/app/shopping-list/edition/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,11 +7,21 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput') name: ElementRef;
+  @ViewChild('amountInput') amount: ElementRef;
+  @Output() ingredientCreated = new EventEmitter<Ingredient>();
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  newIngredient() {
+    // tslint:disable-next-line:radix
+    const elemento = new Ingredient(this.name.nativeElement.value, parseInt(this.amount.nativeElement.value));
+    console.log(elemento);
+    this.ingredientCreated.emit(elemento);
   }
 
 }
